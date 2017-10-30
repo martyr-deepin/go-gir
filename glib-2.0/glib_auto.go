6fd0b0e79b52b4ffe19d6e15b6b3b5ec5e60b0b5
electricface <songwentai@deepin.com>
2017-10-30 18:02:25 +0800

    +1

--- glib-2.0/glib_auto.go
+++ glib-2.0/glib_auto.go
@@ -527,3 +527,31 @@ func SourceSetNameById(tag uint, name string) {
 	defer C.free(unsafe.Pointer(name0)) /*ch:<stdlib.h>*/
 	C.g_source_set_name_by_id(C.guint(tag), name0)
 }
+
+// IdleSourceNew is a wrapper around g_idle_source_new().
+func IdleSourceNew() Source {
+	ret0 := C.g_idle_source_new()
+	return wrapSource(ret0)
+}
+
+// TimeoutSourceNew is a wrapper around g_timeout_source_new().
+func TimeoutSourceNew(interval uint) Source {
+
+	// Var for Go: interval
+	// Var for C: interval0
+	// Type for Go: uint
+	// Type for C: C.guint
+	ret0 := C.g_timeout_source_new(C.guint(interval))
+	return wrapSource(ret0)
+}
+
+// TimeoutSourceNewSeconds is a wrapper around g_timeout_source_new_seconds().
+func TimeoutSourceNewSeconds(interval uint) Source {
+
+	// Var for Go: interval
+	// Var for C: interval0
+	// Type for Go: uint
+	// Type for C: C.guint
+	ret0 := C.g_timeout_source_new_seconds(C.guint(interval))
+	return wrapSource(ret0)
+}
