fb954dab9564df85f6127e5a5f28910f4e618fbc
electricface <songwentai@deepin.com>
2017-10-30 17:31:54 +0800

    合并为一个文件

--- gio-2.0/gio_auto.go
+++ gio-2.0/gio_auto.go
@@ -2,7 +2,6 @@ package gio
 
 /*
 #cgo pkg-config: gio-2.0 gio-unix-2.0
-#include <stdlib.h>
 #include <gio/gdesktopappinfo.h>
 #include <gio/gfiledescriptorbased.h>
 #include <gio/gio.h>
@@ -14,12 +13,39 @@ package gio
 #include <gio/gunixmounts.h>
 #include <gio/gunixoutputstream.h>
 #include <gio/gunixsocketaddress.h>
+#include <stdlib.h>
 */
 import "C"
 import "unsafe"
 import "github.com/electricface/go-auto-gir/gobject-2.0"
 import "github.com/electricface/go-auto-gir/util"
 import "github.com/electricface/go-auto-gir/glib-2.0"
+
+// Interface AppInfo
+type AppInfo struct {
+	Ptr unsafe.Pointer
+}
+
+func (v AppInfo) native() *C.GAppInfo {
+	return (*C.GAppInfo)(v.Ptr)
+}
+func wrapAppInfo(p *C.GAppInfo) AppInfo {
+	return AppInfo{unsafe.Pointer(p)}
+}
+func WrapAppInfo(p unsafe.Pointer) AppInfo {
+	return AppInfo{p}
+}
+
+// GetId is a wrapper around g_app_info_get_id().
+func (appinfo AppInfo) GetId() string {
+
+	// Var for Go: appinfo
+	// Var for C: appinfo0
+	// Type for Go: AppInfo
+	// Type for C: *C.GAppInfo
+	ret0 := C.g_app_info_get_id(appinfo.native())
+	return C.GoString(ret0)
+}
 
 // Object Settings
 type Settings struct {
@@ -36,19 +62,6 @@ func wrapSettings(p *C.GSettings) (v Settings) {
 func WrapSettings(p unsafe.Pointer) (v Settings) {
 	v.Ptr = p
 	return
-}
-
-func (v Settings) GetType() gobject.Type {
-	return gobject.Type(C.g_settings_get_type())
-}
-
-func (v Settings) GetGValueGetter() gobject.GValueGetter {
-	return gvalueGetSettings
-}
-
-func gvalueGetSettings(p unsafe.Pointer) (interface{}, error) {
-	ret := C.g_value_get_object((*C.GValue)(p))
-	return WrapSettings(unsafe.Pointer(ret)), nil
 }
 
 // SettingsNew is a wrapper around g_settings_new().
@@ -271,4 +284,200 @@ func (settings Settings) GetValue(key string) glib.Variant {
 	defer C.free(unsafe.Pointer(key0)) /*ch:<stdlib.h>*/
 	ret0 := C.g_settings_get_value(settings.native(), key0)
 	return glib.WrapVariant(unsafe.Pointer(ret0)) /*gir:glib*/
+}
+
+// Interface File
+type File struct {
+	Ptr unsafe.Pointer
+}
+
+func (v File) native() *C.GFile {
+	return (*C.GFile)(v.Ptr)
+}
+func wrapFile(p *C.GFile) File {
+	return File{unsafe.Pointer(p)}
+}
+func WrapFile(p unsafe.Pointer) File {
+	return File{p}
+}
+
+// GetPath is a wrapper around g_file_get_path().
+func (file File) GetPath() string {
+
+	// Var for Go: file
+	// Var for C: file0
+	// Type for Go: File
+	// Type for C: *C.GFile
+	ret0 := C.g_file_get_path(file.native())
+	defer C.g_free(C.gpointer(ret0))
+	return C.GoString(ret0)
+}
+
+// Replace is a wrapper around g_file_replace().
+func (file File) Replace(etag string, make_backup bool, flags FileCreateFlags, cancellable Cancellable) (FileOutputStream, error) {
+
+	// Var for Go: file
+	// Var for C: file0
+	// Type for Go: File
+	// Type for C: *C.GFile
+
+	// Var for Go: etag
+	// Var for C: etag0
+	// Type for Go: string
+	// Type for C: *C.char
+	etag0 := C.CString(etag)
+	defer C.free(unsafe.Pointer(etag0)) /*ch:<stdlib.h>*/
+
+	// Var for Go: make_backup
+	// Var for C: make_backup0
+	// Type for Go: bool
+	// Type for C: C.gboolean
+
+	// Var for Go: flags
+	// Var for C: flags0
+	// Type for Go: FileCreateFlags
+	// Type for C: C.GFileCreateFlags
+
+	// Var for Go: cancellable
+	// Var for C: cancellable0
+	// Type for Go: Cancellable
+	// Type for C: *C.GCancellable
+	var err glib.Error
+	ret0 := C.g_file_replace(file.native(), etag0, C.gboolean(util.Bool2Int(make_backup)) /*go:.util*/, C.GFileCreateFlags(flags), cancellable.native(), (**C.GError)(unsafe.Pointer(&err)))
+	if err.Ptr != nil {
+		defer err.Free()
+		return FileOutputStream{}, err.GoValue()
+	}
+	return wrapFileOutputStream(ret0), nil
+}
+
+// Object Cancellable
+type Cancellable struct {
+	gobject.Object
+}
+
+func (v Cancellable) native() *C.GCancellable {
+	return (*C.GCancellable)(v.Ptr)
+}
+func wrapCancellable(p *C.GCancellable) (v Cancellable) {
+	v.Ptr = unsafe.Pointer(p)
+	return
+}
+func WrapCancellable(p unsafe.Pointer) (v Cancellable) {
+	v.Ptr = p
+	return
+}
+
+// Object OutputStream
+type OutputStream struct {
+	gobject.Object
+}
+
+func (v OutputStream) native() *C.GOutputStream {
+	return (*C.GOutputStream)(v.Ptr)
+}
+func wrapOutputStream(p *C.GOutputStream) (v OutputStream) {
+	v.Ptr = unsafe.Pointer(p)
+	return
+}
+func WrapOutputStream(p unsafe.Pointer) (v OutputStream) {
+	v.Ptr = p
+	return
+}
+
+// Object FileOutputStream
+type FileOutputStream struct {
+	OutputStream
+}
+
+func (v FileOutputStream) native() *C.GFileOutputStream {
+	return (*C.GFileOutputStream)(v.Ptr)
+}
+func wrapFileOutputStream(p *C.GFileOutputStream) (v FileOutputStream) {
+	v.Ptr = unsafe.Pointer(p)
+	return
+}
+func WrapFileOutputStream(p unsafe.Pointer) (v FileOutputStream) {
+	v.Ptr = p
+	return
+}
+
+// QueryInfo is a wrapper around g_file_output_stream_query_info().
+func (stream FileOutputStream) QueryInfo(attributes string, cancellable Cancellable) (FileInfo, error) {
+
+	// Var for Go: stream
+	// Var for C: stream0
+	// Type for Go: FileOutputStream
+	// Type for C: *C.GFileOutputStream
+
+	// Var for Go: attributes
+	// Var for C: attributes0
+	// Type for Go: string
+	// Type for C: *C.char
+	attributes0 := C.CString(attributes)
+	defer C.free(unsafe.Pointer(attributes0)) /*ch:<stdlib.h>*/
+
+	// Var for Go: cancellable
+	// Var for C: cancellable0
+	// Type for Go: Cancellable
+	// Type for C: *C.GCancellable
+	var err glib.Error
+	ret0 := C.g_file_output_stream_query_info(stream.native(), attributes0, cancellable.native(), (**C.GError)(unsafe.Pointer(&err)))
+	if err.Ptr != nil {
+		defer err.Free()
+		return FileInfo{}, err.GoValue()
+	}
+	return wrapFileInfo(ret0), nil
+}
+
+// Object FileInfo
+type FileInfo struct {
+	gobject.Object
+}
+
+func (v FileInfo) native() *C.GFileInfo {
+	return (*C.GFileInfo)(v.Ptr)
+}
+func wrapFileInfo(p *C.GFileInfo) (v FileInfo) {
+	v.Ptr = unsafe.Pointer(p)
+	return
+}
+func WrapFileInfo(p unsafe.Pointer) (v FileInfo) {
+	v.Ptr = p
+	return
+}
+
+// Object Application
+type Application struct {
+	gobject.Object
+}
+
+func (v Application) native() *C.GApplication {
+	return (*C.GApplication)(v.Ptr)
+}
+func wrapApplication(p *C.GApplication) (v Application) {
+	v.Ptr = unsafe.Pointer(p)
+	return
+}
+func WrapApplication(p unsafe.Pointer) (v Application) {
+	v.Ptr = p
+	return
+}
+
+// ApplicationNew is a wrapper around g_application_new().
+func ApplicationNew(application_id string, flags ApplicationFlags) Application {
+
+	// Var for Go: application_id
+	// Var for C: application_id0
+	// Type for Go: string
+	// Type for C: *C.gchar
+	application_id0 := (*C.gchar)(C.CString(application_id))
+	defer C.free(unsafe.Pointer(application_id0)) /*ch:<stdlib.h>*/
+
+	// Var for Go: flags
+	// Var for C: flags0
+	// Type for Go: ApplicationFlags
+	// Type for C: C.GApplicationFlags
+	ret0 := C.g_application_new(application_id0, C.GApplicationFlags(flags))
+	return wrapApplication(ret0)
 }
--- glib-2.0/glib_auto.go
+++ glib-2.0/glib_auto.go
@@ -3,33 +3,527 @@ package glib
 /*
 #cgo pkg-config: glib-2.0
 #include <glib.h>
+#include <stdlib.h>
 */
 import "C"
+import "unsafe"
+import "github.com/electricface/go-auto-gir/util"
 
-// IdleSourceNew is a wrapper around g_idle_source_new().
-func IdleSourceNew() Source {
-	ret0 := C.g_idle_source_new()
+// Struct Variant
+type Variant struct {
+	Ptr unsafe.Pointer
+}
+
+func (v Variant) native() *C.GVariant {
+	return (*C.GVariant)(v.Ptr)
+}
+func wrapVariant(p *C.GVariant) Variant {
+	return Variant{unsafe.Pointer(p)}
+}
+func WrapVariant(p unsafe.Pointer) Variant {
+	return Variant{p}
+}
+
+// VariantNewBoolean is a wrapper around g_variant_new_boolean().
+func VariantNewBoolean(value bool) Variant {
+
+	// Var for Go: value
+	// Var for C: value0
+	// Type for Go: bool
+	// Type for C: C.gboolean
+	ret0 := C.g_variant_new_boolean(C.gboolean(util.Bool2Int(value)) /*go:.util*/)
+	return wrapVariant(ret0)
+}
+
+// GetType is a wrapper around g_variant_get_type().
+func (value Variant) GetType() VariantType {
+
+	// Var for Go: value
+	// Var for C: value0
+	// Type for Go: Variant
+	// Type for C: *C.GVariant
+	ret0 := C.g_variant_get_type(value.native())
+	return wrapVariantType(ret0)
+}
+
+// IsFloating is a wrapper around g_variant_is_floating().
+func (value Variant) IsFloating() bool {
+
+	// Var for Go: value
+	// Var for C: value0
+	// Type for Go: Variant
+	// Type for C: *C.GVariant
+	ret0 := C.g_variant_is_floating(value.native())
+	return util.Int2Bool(int(ret0)) /*go:.util*/
+}
+
+// Ref is a wrapper around g_variant_ref().
+func (value Variant) Ref() Variant {
+
+	// Var for Go: value
+	// Var for C: value0
+	// Type for Go: Variant
+	// Type for C: *C.GVariant
+	ret0 := C.g_variant_ref(value.native())
+	return wrapVariant(ret0)
+}
+
+// RefSink is a wrapper around g_variant_ref_sink().
+func (value Variant) RefSink() Variant {
+
+	// Var for Go: value
+	// Var for C: value0
+	// Type for Go: Variant
+	// Type for C: *C.GVariant
+	ret0 := C.g_variant_ref_sink(value.native())
+	return wrapVariant(ret0)
+}
+
+// Unref is a wrapper around g_variant_unref().
+func (value Variant) Unref() {
+
+	// Var for Go: value
+	// Var for C: value0
+	// Type for Go: Variant
+	// Type for C: *C.GVariant
+	C.g_variant_unref(value.native())
+}
+
+// Struct VariantType
+type VariantType struct {
+	Ptr unsafe.Pointer
+}
+
+func (v VariantType) native() *C.GVariantType {
+	return (*C.GVariantType)(v.Ptr)
+}
+func wrapVariantType(p *C.GVariantType) VariantType {
+	return VariantType{unsafe.Pointer(p)}
+}
+func WrapVariantType(p unsafe.Pointer) VariantType {
+	return VariantType{p}
+}
+
+// Struct Error
+type Error struct {
+	Ptr unsafe.Pointer
+}
+
+func (v Error) native() *C.GError {
+	return (*C.GError)(v.Ptr)
+}
+func wrapError(p *C.GError) Error {
+	return Error{unsafe.Pointer(p)}
+}
+func WrapError(p unsafe.Pointer) Error {
+	return Error{p}
+}
+
+// Free is a wrapper around g_error_free().
+func (error Error) Free() {
+
+	// Var for Go: error
+	// Var for C: error0
+	// Type for Go: Error
+	// Type for C: *C.GError
+	C.g_error_free(error.native())
+}
+
+// Struct MainLoop
+type MainLoop struct {
+	Ptr unsafe.Pointer
+}
+
+func (v MainLoop) native() *C.GMainLoop {
+	return (*C.GMainLoop)(v.Ptr)
+}
+func wrapMainLoop(p *C.GMainLoop) MainLoop {
+	return MainLoop{unsafe.Pointer(p)}
+}
+func WrapMainLoop(p unsafe.Pointer) MainLoop {
+	return MainLoop{p}
+}
+
+// MainLoopNew is a wrapper around g_main_loop_new().
+func MainLoopNew(context MainContext, is_running bool) MainLoop {
+
+	// Var for Go: context
+	// Var for C: context0
+	// Type for Go: MainContext
+	// Type for C: *C.GMainContext
+
+	// Var for Go: is_running
+	// Var for C: is_running0
+	// Type for Go: bool
+	// Type for C: C.gboolean
+	ret0 := C.g_main_loop_new(context.native(), C.gboolean(util.Bool2Int(is_running)) /*go:.util*/)
+	return wrapMainLoop(ret0)
+}
+
+// GetContext is a wrapper around g_main_loop_get_context().
+func (loop MainLoop) GetContext() MainContext {
+
+	// Var for Go: loop
+	// Var for C: loop0
+	// Type for Go: MainLoop
+	// Type for C: *C.GMainLoop
+	ret0 := C.g_main_loop_get_context(loop.native())
+	return wrapMainContext(ret0)
+}
+
+// IsRunning is a wrapper around g_main_loop_is_running().
+func (loop MainLoop) IsRunning() bool {
+
+	// Var for Go: loop
+	// Var for C: loop0
+	// Type for Go: MainLoop
+	// Type for C: *C.GMainLoop
+	ret0 := C.g_main_loop_is_running(loop.native())
+	return util.Int2Bool(int(ret0)) /*go:.util*/
+}
+
+// Quit is a wrapper around g_main_loop_quit().
+func (loop MainLoop) Quit() {
+
+	// Var for Go: loop
+	// Var for C: loop0
+	// Type for Go: MainLoop
+	// Type for C: *C.GMainLoop
+	C.g_main_loop_quit(loop.native())
+}
+
+// Ref is a wrapper around g_main_loop_ref().
+func (loop MainLoop) Ref() MainLoop {
+
+	// Var for Go: loop
+	// Var for C: loop0
+	// Type for Go: MainLoop
+	// Type for C: *C.GMainLoop
+	ret0 := C.g_main_loop_ref(loop.native())
+	return wrapMainLoop(ret0)
+}
+
+// Run is a wrapper around g_main_loop_run().
+func (loop MainLoop) Run() {
+
+	// Var for Go: loop
+	// Var for C: loop0
+	// Type for Go: MainLoop
+	// Type for C: *C.GMainLoop
+	C.g_main_loop_run(loop.native())
+}
+
+// Unref is a wrapper around g_main_loop_unref().
+func (loop MainLoop) Unref() {
+
+	// Var for Go: loop
+	// Var for C: loop0
+	// Type for Go: MainLoop
+	// Type for C: *C.GMainLoop
+	C.g_main_loop_unref(loop.native())
+}
+
+// Struct MainContext
+type MainContext struct {
+	Ptr unsafe.Pointer
+}
+
+func (v MainContext) native() *C.GMainContext {
+	return (*C.GMainContext)(v.Ptr)
+}
+func wrapMainContext(p *C.GMainContext) MainContext {
+	return MainContext{unsafe.Pointer(p)}
+}
+func WrapMainContext(p unsafe.Pointer) MainContext {
+	return MainContext{p}
+}
+
+// MainContextNew is a wrapper around g_main_context_new().
+func MainContextNew() MainContext {
+	ret0 := C.g_main_context_new()
+	return wrapMainContext(ret0)
+}
+
+// FindSourceById is a wrapper around g_main_context_find_source_by_id().
+func (context MainContext) FindSourceById(source_id uint) Source {
+
+	// Var for Go: context
+	// Var for C: context0
+	// Type for Go: MainContext
+	// Type for C: *C.GMainContext
+
+	// Var for Go: source_id
+	// Var for C: source_id0
+	// Type for Go: uint
+	// Type for C: C.guint
+	ret0 := C.g_main_context_find_source_by_id(context.native(), C.guint(source_id))
 	return wrapSource(ret0)
 }
 
-// TimeoutSourceNew is a wrapper around g_timeout_source_new().
-func TimeoutSourceNew(interval uint) Source {
+// Iteration is a wrapper around g_main_context_iteration().
+func (context MainContext) Iteration(may_block bool) bool {
 
-	// Var for Go: interval
-	// Var for C: interval0
-	// Type for Go: uint
-	// Type for C: C.guint
-	ret0 := C.g_timeout_source_new(C.guint(interval))
+	// Var for Go: context
+	// Var for C: context0
+	// Type for Go: MainContext
+	// Type for C: *C.GMainContext
+
+	// Var for Go: may_block
+	// Var for C: may_block0
+	// Type for Go: bool
+	// Type for C: C.gboolean
+	ret0 := C.g_main_context_iteration(context.native(), C.gboolean(util.Bool2Int(may_block)) /*go:.util*/)
+	return util.Int2Bool(int(ret0)) /*go:.util*/
+}
+
+// Pending is a wrapper around g_main_context_pending().
+func (context MainContext) Pending() bool {
+
+	// Var for Go: context
+	// Var for C: context0
+	// Type for Go: MainContext
+	// Type for C: *C.GMainContext
+	ret0 := C.g_main_context_pending(context.native())
+	return util.Int2Bool(int(ret0)) /*go:.util*/
+}
+
+// Ref is a wrapper around g_main_context_ref().
+func (context MainContext) Ref() MainContext {
+
+	// Var for Go: context
+	// Var for C: context0
+	// Type for Go: MainContext
+	// Type for C: *C.GMainContext
+	ret0 := C.g_main_context_ref(context.native())
+	return wrapMainContext(ret0)
+}
+
+// Unref is a wrapper around g_main_context_unref().
+func (context MainContext) Unref() {
+
+	// Var for Go: context
+	// Var for C: context0
+	// Type for Go: MainContext
+	// Type for C: *C.GMainContext
+	C.g_main_context_unref(context.native())
+}
+
+// MainContextDefault is a wrapper around g_main_context_default().
+func MainContextDefault() MainContext {
+	ret0 := C.g_main_context_default()
+	return wrapMainContext(ret0)
+}
+
+// Struct Source
+type Source struct {
+	Ptr unsafe.Pointer
+}
+
+func (v Source) native() *C.GSource {
+	return (*C.GSource)(v.Ptr)
+}
+func wrapSource(p *C.GSource) Source {
+	return Source{unsafe.Pointer(p)}
+}
+func WrapSource(p unsafe.Pointer) Source {
+	return Source{p}
+}
+
+// Attach is a wrapper around g_source_attach().
+func (source Source) Attach(context MainContext) uint {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+
+	// Var for Go: context
+	// Var for C: context0
+	// Type for Go: MainContext
+	// Type for C: *C.GMainContext
+	ret0 := C.g_source_attach(source.native(), context.native())
+	return uint(ret0)
+}
+
+// Destroy is a wrapper around g_source_destroy().
+func (source Source) Destroy() {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+	C.g_source_destroy(source.native())
+}
+
+// GetCanRecurse is a wrapper around g_source_get_can_recurse().
+func (source Source) GetCanRecurse() bool {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+	ret0 := C.g_source_get_can_recurse(source.native())
+	return util.Int2Bool(int(ret0)) /*go:.util*/
+}
+
+// GetContext is a wrapper around g_source_get_context().
+func (source Source) GetContext() MainContext {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+	ret0 := C.g_source_get_context(source.native())
+	return wrapMainContext(ret0)
+}
+
+// GetId is a wrapper around g_source_get_id().
+func (source Source) GetId() uint {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+	ret0 := C.g_source_get_id(source.native())
+	return uint(ret0)
+}
+
+// GetName is a wrapper around g_source_get_name().
+func (source Source) GetName() string {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+	ret0 := C.g_source_get_name(source.native())
+	return C.GoString(ret0)
+}
+
+// GetPriority is a wrapper around g_source_get_priority().
+func (source Source) GetPriority() int {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+	ret0 := C.g_source_get_priority(source.native())
+	return int(ret0)
+}
+
+// GetTime is a wrapper around g_source_get_time().
+func (source Source) GetTime() int64 {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+	ret0 := C.g_source_get_time(source.native())
+	return int64(ret0)
+}
+
+// IsDestroyed is a wrapper around g_source_is_destroyed().
+func (source Source) IsDestroyed() bool {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+	ret0 := C.g_source_is_destroyed(source.native())
+	return util.Int2Bool(int(ret0)) /*go:.util*/
+}
+
+// Ref is a wrapper around g_source_ref().
+func (source Source) Ref() Source {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+	ret0 := C.g_source_ref(source.native())
 	return wrapSource(ret0)
 }
 
-// TimeoutSourceNewSeconds is a wrapper around g_timeout_source_new_seconds().
-func TimeoutSourceNewSeconds(interval uint) Source {
+// SetCanRecurse is a wrapper around g_source_set_can_recurse().
+func (source Source) SetCanRecurse(can_recurse bool) {
 
-	// Var for Go: interval
-	// Var for C: interval0
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+
+	// Var for Go: can_recurse
+	// Var for C: can_recurse0
+	// Type for Go: bool
+	// Type for C: C.gboolean
+	C.g_source_set_can_recurse(source.native(), C.gboolean(util.Bool2Int(can_recurse)) /*go:.util*/)
+}
+
+// SetName is a wrapper around g_source_set_name().
+func (source Source) SetName(name string) {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+
+	// Var for Go: name
+	// Var for C: name0
+	// Type for Go: string
+	// Type for C: *C.char
+	name0 := C.CString(name)
+	defer C.free(unsafe.Pointer(name0)) /*ch:<stdlib.h>*/
+	C.g_source_set_name(source.native(), name0)
+}
+
+// SetPriority is a wrapper around g_source_set_priority().
+func (source Source) SetPriority(priority int) {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+
+	// Var for Go: priority
+	// Var for C: priority0
+	// Type for Go: int
+	// Type for C: C.gint
+	C.g_source_set_priority(source.native(), C.gint(priority))
+}
+
+// Unref is a wrapper around g_source_unref().
+func (source Source) Unref() {
+
+	// Var for Go: source
+	// Var for C: source0
+	// Type for Go: Source
+	// Type for C: *C.GSource
+	C.g_source_unref(source.native())
+}
+
+// SourceRemove is a wrapper around g_source_remove().
+func SourceRemove(tag uint) bool {
+
+	// Var for Go: tag
+	// Var for C: tag0
 	// Type for Go: uint
 	// Type for C: C.guint
-	ret0 := C.g_timeout_source_new_seconds(C.guint(interval))
-	return wrapSource(ret0)
+	ret0 := C.g_source_remove(C.guint(tag))
+	return util.Int2Bool(int(ret0)) /*go:.util*/
+}
+
+// SourceSetNameById is a wrapper around g_source_set_name_by_id().
+func SourceSetNameById(tag uint, name string) {
+
+	// Var for Go: tag
+	// Var for C: tag0
+	// Type for Go: uint
+	// Type for C: C.guint
+
+	// Var for Go: name
+	// Var for C: name0
+	// Type for Go: string
+	// Type for C: *C.char
+	name0 := C.CString(name)
+	defer C.free(unsafe.Pointer(name0)) /*ch:<stdlib.h>*/
+	C.g_source_set_name_by_id(C.guint(tag), name0)
 }
