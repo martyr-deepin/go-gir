0bfd984cc155351318581b7dbfd1f709ab01d06c
electricface <songwentai@deepin.com>
2017-11-03 21:34:14 +0800

    改进函数 GetZeroTermArrayLen

--- util/util.go
+++ util/util.go
@@ -28,16 +28,56 @@ func GetZeroTermArrayLen(p unsafe.Pointer, size uintptr) int {
 		return 0
 	}
 	var count uintptr
-	//fmt.Println("size is", size)
-	for {
-		value := (*[999]byte)(unsafe.Pointer(uintptr(p) + (count * size)))
-		//fmt.Println("value is", value)
-		if allZero(value, size) {
-			break
+	//println("size is", size)
+	switch size {
+	case 8:
+		for {
+			value := *(*int64)(unsafe.Pointer(uintptr(p) + (count * size)))
+			if value == 0 {
+				break
+			}
+			count++
 		}
-		count++
-	}
 
+	case 4:
+		for {
+			value := *(*int32)(unsafe.Pointer(uintptr(p) + (count * size)))
+			if value == 0 {
+				break
+			}
+			count++
+		}
+
+	case 2:
+		for {
+			value := *(*int16)(unsafe.Pointer(uintptr(p) + (count * size)))
+			if value == 0 {
+				break
+			}
+			count++
+		}
+
+	case 1:
+		for {
+			value := *(*int8)(unsafe.Pointer(uintptr(p) + (count * size)))
+			if value == 0 {
+				break
+			}
+			count++
+		}
+
+	case 0:
+		panic("assert failed size != 0")
+
+	default:
+		for {
+			value := (*[999]byte)(unsafe.Pointer(uintptr(p) + (count * size)))
+			if allZero(value, size) {
+				break
+			}
+			count++
+		}
+	}
 	return int(count)
 }
 
